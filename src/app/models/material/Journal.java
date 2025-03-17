38d865f1b4c27c6f10a149e536a5efbb114e6c70
Mateo Manchola <mmanchola12temporal@gmail.com>
2025-03-17 03:52:53 +0100

    Journal class implemented

--- src/app/models/material/Journal.java
+++ src/app/models/material/Journal.java
@@ -1,5 +1,31 @@
 package app.models.material;
 
-public class Journal {
+public class Journal extends Material {
+    private int issue;
+
+    public Journal(String title, String author, int published, int issue) {
+        super(title, author, published);
+        this.issue = issue;
+    }
+
+    public int getIssue() {
+        return issue;
+    }
+
+    public void setIssue(int issue) {
+        this.issue = issue;
+    }
+
+    @Override
+    public boolean lend() {
+        boolean lend = this.lend;
+        this.lend = true;
+        return lend;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Revista:%n %s%n\tGenero: %s", super.toString(), this.issue);
+    }
 
 }
